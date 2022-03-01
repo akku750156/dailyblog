cbcbefd48e676046bbc1fff24ac6fac8c1cc249e
akku750156 <akashtiwari750156@gmail.com>
2022-03-01 12:39:21 +0530

    Title added on Create,Update,Detail page

--- components/Home/Blog.js
+++ components/Home/Blog.js
@@ -2,19 +2,24 @@ import React from "react";
 import Image from "next/image";
 
 function Blog({ post }) {
-  const url =
-    post.picture ||
-    "https://cdn.pixabay.com/photo/2012/08/27/14/19/mountains-55067_1280.png";
-
   return (
     <div className="flex flex-col justify-center items-start w-full p-4">
       <div className="w-full lg:h-72 md:h-60 sm:h-48 h-32 mb-4 relative">
-        <Image
-          layout="fill"
-          src={url}
-          alt=""
-          className="rounded-2xl drop-shadow-xl w-full h-full object-cover"
-        />
+        {post.picture ? (
+          <Image
+            layout="fill"
+            src={post.picture}
+            alt=""
+            className="rounded-2xl drop-shadow-xl w-full h-full object-cover"
+          />
+        ) : (
+          <Image
+            layout="fill"
+            src="/blogImage4.jpg"
+            alt=""
+            className="rounded-2xl drop-shadow-xl w-full h-full object-cover"
+          />
+        )}
       </div>
       <div className="text-gray-500 text-sm">{post.categories}</div>
       <div className="text-lg lg:text-2xl my-2 font-bold">{post.title}</div>
