3750431d0783db635c30e791d09a89aec65ac8dc
akku750156 <akashtiwari750156@gmail.com>
2022-03-03 02:42:36 +0530

    HOVER: Hover effect added on blogs cards

--- components/Home/Blog.js
+++ components/Home/Blog.js
@@ -3,26 +3,26 @@ import Image from "next/image";
 
 function Blog({ post }) {
   return (
-    <div className="flex flex-col justify-center items-start w-full p-4">
-      <div className="w-full lg:h-72 md:h-60 sm:h-48 h-32 mb-4 relative">
+    <div className="flex flex-col justify-center items-start w-full p-2 md:p-4">
+      <div className="w-full lg:h-72 md:h-60 sm:h-48 h-40 mb-4 relative shadow-lg shadow-gray-700">
         {post.picture ? (
           <Image
             layout="fill"
             src={post.picture}
             alt=""
-            className="rounded-2xl drop-shadow-xl w-full h-full object-cover"
+            className="rounded-lg  w-full h-full object-cover"
           />
         ) : (
           <Image
             layout="fill"
             src="/blogImage4.jpg"
             alt=""
-            className="rounded-2xl drop-shadow-xl w-full h-full object-cover"
+            className="rounded-lg   w-full h-full object-cover"
           />
         )}
       </div>
       <div className="text-gray-500 text-sm">{post.categories}</div>
-      <div className="text-lg lg:text-2xl my-2 font-bold">{post.title}</div>
+      <div className="text-lg lg:text-2xl my-1 font-bold">{post.title}</div>
       <div className="text-gray-500 text-sm">Author : {post.username}</div>
     </div>
   );
