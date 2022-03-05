63d9bfe9344aa1aabf7c2f62fa05fa936e57fdf7
akku750156 <akashtiwari750156@gmail.com>
2022-03-05 16:34:14 +0530

    Authentication done

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export const getServerSideProps = async (context) => {
   if (!session) {
     return {
       redirect: {
-        destination: "/auth",
+        destination: "/",
         permanent: false,
       },
     };
